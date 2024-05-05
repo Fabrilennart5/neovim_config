4b8e8e56cab2ad483834a7c82f35f2f0b58f9066
Fabricio Lennart Flores Ledezma <fabricioflores515@gmail.com>
2024-05-04 23:25:38 -0400

    nvim config mayo 4 de 2024

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,75 @@
+-- EXAMPLE
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "ruff_lsp", "terraform_lsp", "rust_analyzer", "sqlls", "pyright", "taplo" }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- typescript
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- configuracion apika
+local function lspSymbol(name, icon)
+  local hl = "DiagnosticSign" .. name
+  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
+
+lspSymbol("Error", "󰅙")
+lspSymbol("Info", "󰋼")
+lspSymbol("Hint", "󰌵")
+lspSymbol("Warn", "")
+
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = "",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  virtual_text = {
+    spacing = 5,
+    min = { severity = "Warning" },
+  },
+  update_in_insert = true,
+})
+
+lspconfig.lua_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
---
+++ lua/plugins/init.lua
@@ -0,0 +1,96 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre' -- uncomment for format on save
+    enabled = false,
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "prettier",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+      },
+    },
+  },
+  {
+    "kyazdani42/nvim-tree.lua",
+    lazy = true,
+    event = "BufRead",
+    cmd = { "NvimTree", "NvimTreeToggle", "NvimTreeFocus", "NvimTreeClose" },
+    dependencies = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require "configs.user.nvimtree"
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = { "BufRead", "BufNewFile", "InsertEnter" },
+    config = function()
+      require "configs.user.lualine"
+    end,
+  },
+  {
+    "jayp0521/mason-null-ls.nvim",
+    lazy = true,
+    dependencies = {
+      "nvimtools/none-ls.nvim",
+      dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+        lazy = true,
+      },
+      config = function()
+        require "configs.user.null_ls"
+      end,
+    },
+    event = "InsertEnter",
+    opts = function()
+      require "configs.user.mason-null-ls"
+    end,
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    event = "BufRead",
+    config = function()
+      require "configs.user.smartsplit"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "BufRead",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    cmd = "ToggleTerm",
+    event = "BufRead",
+    config = function()
+      require "configs.user.toggleterm"
+    end,
+  },
+}
